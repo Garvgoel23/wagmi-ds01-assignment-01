00fb4018939e10be2e247a8f3a737d62631a967d
Garv Goel <goelgarv99@gmail.com>
2025-10-22 19:16:07 +0530

    Budget done

--- src/main/java/com/wagmi/finance/strings/StringUtils.java
+++ src/main/java/com/wagmi/finance/strings/StringUtils.java
@@ -1,4 +1,8 @@
+
 package main.java.com.wagmi.finance.strings;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
 
 /*
  TODO[Student]: String utilities
@@ -13,13 +17,51 @@ public final class StringUtils {
     }
 
     public static String sanitizeDescription(String input) {
-
         // stub: trim, collapse spaces, remove invalid chars
-        throw new UnsupportedOperationException("Not implemented");
+        if (input == null ){
+            throw new NullPointerException("Input string cannot be null.");}
+        if(input.isEmpty()) {
+            return "";
+        }
+        StringBuilder ans=new StringBuilder();
+        boolean previousCharWasSpace = false;
+        for(int i=0;i<input.length();i++){
+            if(Character.isLetterOrDigit(input.charAt(i))){
+                ans.append(input.charAt(i));
+                previousCharWasSpace = false;
+            }
+            else{
+                if (!previousCharWasSpace) {
+                    // Append a single space character
+                    ans.append(' ');
+                    previousCharWasSpace = true;
+                }
+            }
+        }
+        return ans.toString().trim();
     }
 
     public static boolean matchesDatePattern(String input) {
         // stub: check pattern YYYY-MM-DD
-        throw new UnsupportedOperationException("Not implemented");
+        if(input==null||input.length()!=10)
+        {
+            return false;
+        }
+
+        if (!input.matches("^\\d{4}-\\d{2}-\\d{2}$")) {
+            return false;
+        }
+        int yy=Integer.parseInt(input.substring(0, 4));
+        if(yy<=0)
+        {
+            return false;
+        }
+        try {
+            LocalDate.parse(input);
+            return true;
+        } catch (DateTimeParseException e) {
+            return false;
+        }
+
     }
 }
